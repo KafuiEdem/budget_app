963a8844b6ab23ff3d78797703f5d5107489255f
Edem Robin <robinedemamedzo@gmail.com>
2023-07-20 12:06:45 +0000

    Final part of this project completed

--- budget.py
+++ budget.py
@@ -3,10 +3,12 @@ class Category:
         self.item = item
         self.ledger = []
 
+    #setting the deposit method
     def deposit(self, amount, description=""):
         deposit_item = {"amount": amount, "description": description}
         self.ledger.append(deposit_item)
 
+    #setting the withdarw method
     def withdraw(self, amount, description=""):
         if self.check_funds(amount):
             withdrawal_item = {"amount": -amount, "description": description}
@@ -14,7 +16,8 @@ class Category:
             return True
         else:
             return False
-
+        
+    #setting the get balance method
     def get_balance(self):
         balance = 0
         for entry in self.ledger:
@@ -22,30 +25,33 @@ class Category:
                 balance += entry["amount"]
         return balance
     
-
+    #setting the check funds method 
     def check_funds(self, amount):
         return amount <= self.get_balance()
     
+    #setting the transfer method
     def transfer(self, amount, budget_item):
         if self.check_funds(amount):
-            transfer_to = {"transfer": {"amount": -amount, "description": f"Transfer to {budget_item.item}"}}
-            self.ledger.append(transfer_to)
-            deposit_item = {"deposit": {"amount": amount, "description": f"Transfer from {self.item}"}}
+            transfer_item = {"amount": -amount, "description": f"Transfer to {budget_item.item}"}
+            deposit_item = {"amount": amount, "description": f"Transfer from {self.item}"}
+            self.ledger.append(transfer_item)
             budget_item.ledger.append(deposit_item)
             return True
         else:
             return False
 
+    #setting the the __str__ method for the output
     def __str__(self):
         motd = f"{self.item:*^30}"
         items = ""
         total = 0
 
         for entry in self.ledger:
-            description = entry["description"]
-            amount = entry["amount"]
-            items += f"\n{description[:23]:23}{amount:>7.2f}"
-            total += amount
+            if "description" in entry:
+                description = entry["description"]
+                amount = entry["amount"]
+                items += f"\n{description[:23]:23}{amount:>7.2f}"
+                total += amount
 
         result = motd + items + f"\nTotal: {total:.2f}"
         return result
@@ -56,8 +62,7 @@ def create_spend_chart(categories):
     for category in categories:
         total_spent[category.item] = sum(
             entry["amount"]
-            for entry in category.ledger
-            if entry["amount"] < 0
+            for entry in category.ledger if entry.get("amount",0) < 0
         )
 
     # Calculate the percentage spent for each category
@@ -96,38 +101,51 @@ def create_spend_chart(categories):
     return chart
 
 
-
-food = Category("Food")
-entertainment = Category("Entertainment")
-business = Category("business")
-
-
-food.deposit(900, "deposit")
-entertainment.deposit(900, "deposit")
-business.deposit(900, "deposit")
-food.withdraw(105.55)
-entertainment.withdraw(33.40)
-business.withdraw(10.99)
-c=create_spend_chart([business, food, entertainment])
-print(c)
-
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-transfer_amount = 20
-food_balance_before = food.get_balance()
-entertainment_balance_before = entertainment.get_balance()
-good_transfer = food.transfer(transfer_amount, entertainment)
-food_balance_after = food.get_balance()
-entertainment_balance_after = entertainment.get_balance()
+"""
+    I have to run the test items to see where the error is.
+    The difficult part of this project is the transfer method.
+    This method was behaving very funny, but i manage to fix it.
+"""
+# food = Category("Food")
+# entertainment = Category("Entertainment")
+# business = Category("business")
 
 
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-transfer_amount = 20
-food_balance_before = food.get_balance()
-entertainment_balance_before = entertainment.get_balance()
-good_transfer = food.transfer(transfer_amount, entertainment)
-food_balance_after = food.get_balance()
-entertainment_balance_after = entertainment.get_balance()
-food.ledger[2]
-# print(food)
+# food.deposit(900, "deposit")
+# entertainment.deposit(900, "deposit")
+# business.deposit(900, "deposit")
+# food.withdraw(105.55)
+# entertainment.withdraw(33.40)
+# business.withdraw(10.99)
+# c=create_spend_chart([business, food, entertainment])
+# print(c)
+
+# food.deposit(900, "deposit")
+# food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+# transfer_amount = 20
+# food_balance_before = food.get_balance()
+# entertainment_balance_before = entertainment.get_balance()
+# good_transfer = food.transfer(transfer_amount, entertainment)
+# food_balance_after = food.get_balance()
+# entertainment_balance_after = entertainment.get_balance()
+
+
+# food.deposit(900, "deposit")
+# food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+# transfer_amount = 20
+# food_balance_before = food.get_balance()
+# entertainment_balance_before = entertainment.get_balance()
+# good_transfer = food.transfer(transfer_amount, entertainment)
+# food_balance_after = food.get_balance()
+# entertainment_balance_after = entertainment.get_balance()
+# food.ledger[2]
+# # print(food)
+
+# transfer_amount = 20
+# good_transfer = food.transfer(transfer_amount, entertainment)
+# actual_ledger_item = food.ledger[-1]  # Get the last item in the ledger
+# expected_ledger_item = {
+#     "transfer": {"amount": -20, "description": f"Transfer to {entertainment.item}"}
+# }
+# print("Actual ledger item:", actual_ledger_item)
+# print("Expected ledger item:", expected_ledger_item)
